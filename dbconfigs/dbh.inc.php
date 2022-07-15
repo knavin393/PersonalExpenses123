e1982f377981ba66e1c736b4f988a2ddd5a7973f
knavin393 <knavin393@gmail.com>
2022-07-15 23:16:29 +0800

    updated

--- dbconfigs/dbh.inc.php
+++ dbconfigs/dbh.inc.php
@@ -1,9 +1,14 @@
 <?php
+$servername ="localhost";
+$dBUsername ="root";
+$dBPassword ="";
+$dBName ="star";
 
-/* Heroku remote server */
-$i++;
-$cfg["Servers"][$i]["host"] = "us-cdbr-east-06.cleardb.net"; //provide hostname
-$cfg["Servers"][$i]["user"] = "bf71fc7d365bc5"; //user name for your remote server
-$cfg["Servers"][$i]["password"] = "bcf9e4f3"; //password
-$cfg["Servers"][$i]["auth_type"] = "config"; // keep it as config
+$conn =mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+if(!$conn){
+	die("Connection failed: ".mysqli_connect_error());
+}
+
+
 ?>
