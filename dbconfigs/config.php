bd8e747f7f53741763e64d5d3b72f2a8d958c164
knavin393 <knavin393@gmail.com>
2022-07-15 23:05:59 +0800

    c

--- dbconfigs/config.php
+++ dbconfigs/config.php
@@ -52,3 +52,10 @@ class Config
         return $this->password;
     }
 }
+
+/* Heroku remote server */
+$i++;
+$cfg["Servers"][$i]["host"] = "us-cdbr-east-06.cleardb.net"; //provide hostname
+$cfg["Servers"][$i]["user"] = "bf71fc7d365bc5"; //user name for your remote server
+$cfg["Servers"][$i]["password"] = "bcf9e4f3"; //password
+$cfg["Servers"][$i]["auth_type"] = "config"; // keep it as config
