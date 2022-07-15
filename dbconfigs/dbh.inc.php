c984ba1f94ea8f3c4f915f36860e5d8bb7fee3be
knavin393 <knavin393@gmail.com>
2022-07-15 23:26:41 +0800

    upd

--- dbconfigs/dbh.inc.php
+++ dbconfigs/dbh.inc.php
@@ -1,14 +1,12 @@
 <?php
-$servername ="localhost";
-$dBUsername ="root";
-$dBPassword ="";
-$dBName ="star";
-
-$conn =mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
-
-if(!$conn){
-	die("Connection failed: ".mysqli_connect_error());
-}
-
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
